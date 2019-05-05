1effbfb82f7987b6e588775096028f805b35d476
javivi235 <javisoruco235@gmail.com>
2019-05-05 17:11:40 -0400

    algunos datos extras

--- src/store.js
+++ src/store.js
@@ -5,11 +5,12 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    cuentas: [{ nombre: 'ahorros', fondos: 0 }],
-    categoriaIngresos: ['Salario', 'Transferencia', 'Otros'],
-    categoriaEgresos: ['Expensas', 'Transferencia', 'Otros'],
-    ingresos: [],
-    egresos: []
+    cuentas: [ { nombre: 'ahorros', fondos: 100 } ],
+    categoriaIngresos: [ 'Salario', 'Transferencia', 'Otros' ],
+    categoriaEgresos: [ 'Expensas', 'Transferencia', 'Otros' ],
+    ingresos: [ { cuenta: 'ahorros', fecha: '01/05/2019', monto: 100, categoria: 'Salario' },
+      { cuenta: 'ahorros', fecha: '01/05/2019', monto: 100, categoria: 'Otros' } ],
+    egresos: [ { cuenta: 'ahorros', fecha: '03/05/2019', monto: 100, categoria: 'Expensas' } ]
   },
   mutations: {
   },
