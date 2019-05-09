30efceb8e8575750006e26ebf9b2d15bc8fd48f3
javivi235 <javisoruco235@gmail.com>
2019-05-09 19:54:41 -0400

    tratando de hacer merge prueba 1

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <v-content>
+      <Home/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import Home from '@/views/Home.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Home,
+  },
+  data() {
+    return {
+      //
+    }
+  },
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <CuentaManager :cuenta="cuenta"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import CuentaManager from '@/components/CuentaManager.vue'
+
+export default {
+  name: 'home',
+  components: {
+    CuentaManager,
+  },
+  computed: {
+    cuenta() {
+      return { nombre: 'ahorros', fondos: '100' }
+    },
+  },
+}
+</script>
